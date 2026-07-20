8646c05210a41962d559167d54e9f2efefdbf270
Alex Nicolaou <anicolao@gmail.com>
2026-07-20 08:37:35 -0400

    Allow both Quarry replay versions

--- tests/unit/firebase-rules.test.ts
+++ tests/unit/firebase-rules.test.ts
@@ -10,10 +10,13 @@ test('Realtime Database exposes only the unified controller record journal', () 
   assert.equal(playerRules.progress, undefined);
   assert.equal(playerRules.$other['.validate'], false);
   assert.ok(rules.rules.games.$gameId.winner);
+  assert.match(rules.rules.games.$gameId.start['.validate'], /quarry-match\/1/);
   assert.match(rules.rules.games.$gameId.start['.validate'], /quarry-match\/2/);
   assert.match(rules.rules.games.$gameId.start['.validate'], /crystal-canopy\/1/);
   assert.match(rules.rules.games.$gameId.start['.validate'], /stax\/1/);
   assert.match(playerRules.records.$recordId['.validate'], /c1-/);
+  assert.match(playerRules.records.$recordId['.validate'], /q1-/);
+  assert.match(playerRules.records.$recordId['.validate'], /q2-/);
   assert.match(playerRules.records.$recordId['.validate'], /input\/fire/);
   assert.match(playerRules.records.$recordId['.validate'], /input\/throw-back/);
   assert.match(playerRules.records.$recordId['.validate'], /s1-/);
