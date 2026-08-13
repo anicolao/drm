353bf1f2b643e501060a9a9d48afbf41477aa796
Alex Nicolaou <anicolao@gmail.com>
2026-08-13 17:08:24 -0400

    Stabilize main E2E render checkpoints

---
+++ src/lib/components/stax/StaxRenderCheckpoint.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { useTask, useThrelte } from '@threlte/core';
+
+  export let progress: number | '';
+  export let rendered: (progress: number | '') => void;
+
+  const { autoRenderTask } = useThrelte();
+  useTask(() => rendered(progress), {
+    after: autoRenderTask,
+    autoInvalidate: false,
+  });
+</script>
